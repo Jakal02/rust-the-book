7532b432635082b5346ea717dc11d59f2b97b761
Jakal02 <gitcontribs@eligrubbs.com>
2024-04-25 10:38:50 -0400

    rectangle method to compare with other rectangles.

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -9,6 +9,10 @@ impl Rectangle{
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 fn main() {
     println!("Print the area of a rectangle.");
@@ -21,5 +25,15 @@ fn main() {
 
     println!("The Area is {}", area);
     println!("Rect first: {:?}", rect1);
-    dbg!(rect1);
+    dbg!(&rect1);
+
+    let rect2 = Rectangle {
+        width: 19,
+        height: 29
+    };
+
+    println!("Can rect1 fit rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 fit rect1? {}", rect1.can_hold(&rect1));
+    println!("Can rect2 fit rect1? {}", rect2.can_hold(&rect1));
+
 }
