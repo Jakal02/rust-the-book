1c2f5144be5b9f1aa32a88932df46e7fb0dc5b55
Jakal02 <gitcontribs@eligrubbs.com>
2024-05-11 23:15:48 -0400

    created tests for search function.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -4,6 +4,10 @@ use std::fs;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents: String = fs::read_to_string(config.file_path)?;
 
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
+
     Ok(())
 }
 
@@ -21,5 +25,32 @@ impl Config {
         let file_path: String = args[2].clone();
 
         Ok(Config {query, file_path})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
