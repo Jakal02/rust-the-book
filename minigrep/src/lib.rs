ec35f1e16720d31d8e0cec7fee25395ed39f1619
Jakal02 <gitcontribs@eligrubbs.com>
2024-06-26 16:04:37 -0400

    iterator search algorithm.

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -50,13 +50,9 @@ impl Config {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results: Vec<&str> = Vec::new();
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-    results
+    contents.lines()
+            .filter(|line: &&str| line.contains(query))
+            .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str>{
